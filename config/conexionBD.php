2417c69a8e931f98f162e38741ea23da420e2e39
RobertoPacho <robertsonam1993@gmail.com>
2019-11-26 09:42:51 -0500

    Create conexionBD.php

---
+++ config/conexionBD.php
@@ -0,0 +1,13 @@
+<?php 
+$db_servername="localhost";
+$db_username="root";
+$db_password="";
+$db_name="hypermedial";
+
+$conn = new mysqli($db_servername,$db_username,$db_password,$db_name);
+$conn->set_charset("utf8");
+
+if($conn->connect_error){
+    die("Conexion fallida".$conn->connect_error);
+}
+?> 
